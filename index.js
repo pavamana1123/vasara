dd20a0fe0659e151a346947d3a69ca40dfa2d4ec
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-06-20 21:13:55 +0530

    ok

---
+++ index.js
@@ -0,0 +1,180 @@
+const fs = require('fs')
+const moment = require('moment')
+
+const rtuMap = {
+    "Margasirsa": "Hemanta",
+    "Pausa": "Hemanta",
+    "Magha": "Shishira",
+    "Phalguna": "Shishira",
+    "Caitra": "Vasant",
+    "Vaisakha": "Vasant",
+    "Jyestha": "Grishma",
+    "Asadha": "Grishma",
+    "Sravana": "Varsha",
+    "Bhadra": "Varsha",
+    "Asvina": "Sharat",
+    "Kartika": "Sharat"
+}
+
+const vedicWeekdays = {
+    Sunday: 'bhanu',
+    Monday: 'indu',
+    Tuesday: 'bhauma',
+    Wednesday: 'saumya',
+    Thursday: 'guru',
+    Friday: 'bhargava',
+    Saturday: 'sthira'
+  }
+
+function findIndicesInHeader(headerLine) {
+    const startIndexTithi = headerLine.indexOf("TITHI");
+    const endIndexTithi = headerLine.indexOf("PAKSA") - 1;
+    const startIndexPaksa = endIndexTithi + 1;
+    const endIndexPaksa = headerLine.indexOf("YOGA") - 1;
+    const startIndexYoga = endIndexPaksa + 1;
+    const endIndexYoga = headerLine.indexOf("NAKSATRA") - 1;
+    const startIndexNaksatra = endIndexYoga + 1;
+    const endIndexNaksatra = headerLine.indexOf("FAST") - 1;
+    return [startIndexTithi, endIndexPaksa, startIndexNaksatra, startIndexYoga];
+}
+
+function isHeader(line) {
+    line = line.trim()
+    const regex = /^DATE\s+TITHI\s+PAKSA\s+YOGA\s+NAKSATRA\s+FAST$/;
+    return regex.test(line);
+}
+
+function extractHeader(lines) {
+    for (const line of lines) {
+      if (isHeader(line)) {
+        return line;
+      }
+    }
+    return null; // Return null if header line is not found
+}
+
+function isMasaLine(line) {
+    line = line.trim()
+    const regex = /^([A-Za-z\s-]+)(?:\(([A-Za-z]+)\))?\sMasa,\sGaurabda\s\d+\s+GCal\s\d+,\sBuild\s\d+$/;
+    return regex.test(line);
+}
+  
+function extractMasaDetails(line) {
+    line = line.trim()
+    const regex = /^([A-Za-z\s-]+)(?:\(([A-Za-z]+)\))?\sMasa,\sGaurabda\s\d+\s+GCal\s\d+,\sBuild\s\d+$/;
+    const match = line.match(regex);
+    if (match && match.length >= 2) {
+        const gaudiya = match[1].trim();
+        const traditional = match[2] || gaudiya;
+        return {
+        gaudiya,
+        traditional
+        };
+    }
+    return null;
+}
+
+function isDateLine(line) {
+    line = line.trim()
+    const regex = /^\d{1,2}\s\w+\s\d{4}/;
+    return regex.test(line);
+}
+
+function extractDate(line) {
+    line = line.trim()
+    const regex = /^(\d{1,2}\s\w+\s\d{4})/;
+    const match = line.match(regex);
+    return match ? match[1] : null;
+}
+
+function isSankrantiLine(line) {
+    return line.includes("Makara Sankranti (Sun enters") || line.includes("Karka Sankranti (Sun enters")
+}
+
+function getAyana(line){
+    return line.includes("Makara Sankranti (Sun enters")?"Uttarayana":"Dakshinayana"
+}
+
+function isRemarksLine(line, numSpaces) {
+    const regex = new RegExp(`^\\s{${numSpaces}}`)
+    return regex.test(line) && line[numSpaces]!=" "
+}
+
+// Read the calendar text from file
+fs.readFile('cal.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err)
+    return
+  }
+
+  var masa = null
+  var ayana = "Uttarayana"
+
+  // Split the text into lines
+  const lines = data.split('\n')
+  const [tithiIndex, pakshaIndex, nakshatraIndex ,yogaIndex] = findIndicesInHeader(extractHeader(lines))
+
+  // Define an array to store the parsed calendar entries
+  const calendarEntries = []
+
+  // Iterate over the calendar data
+  for (var line of lines) {
+
+    if(isMasaLine(line)){
+        masa = extractMasaDetails(line)
+        continue
+    }
+
+    if(isSankrantiLine(line)){
+        ayana = getAyana(line)
+        calendarEntries[calendarEntries.length-1].ayana=ayana
+        continue
+    }
+
+    if(isRemarksLine(line, tithiIndex)){
+        calendarEntries[calendarEntries.length-1].remarks = (calendarEntries[calendarEntries.length-1].remarks || [])
+        calendarEntries[calendarEntries.length-1].remarks.push(line.trim())
+        continue
+    }
+
+    if(!isDateLine(line)){
+        // console.log(line)
+        continue
+    }
+
+    var rawDate = extractDate(line)
+    const dayOfWeek = moment(rawDate, 'D MMM YYYY').format('dddd')
+    const formattedDate = moment(rawDate, 'D MMM YYYY').format('YYYY-MM-DD')
+
+    const entry = {
+      date: formattedDate,
+      day: dayOfWeek,
+      ayana,
+      rtu: rtuMap[masa.traditional],
+      gaudiyaMasa: (masa.gaudiya || null),
+      traditionalMasa: (masa.traditional || null),
+      gaudiyaPaksha: line.charAt(pakshaIndex)=="G"?"Gaura":"Krishna",
+      traditionalPaksha: line.charAt(pakshaIndex)=="G"?"Shukla":"Krishna",
+      tithi: line.substring(tithiIndex, pakshaIndex-1).trim(),
+      vasara: vedicWeekdays[dayOfWeek],
+      nakshatra: line.substring(nakshatraIndex).replace(/\*/, '').trim(),
+      yoga: line.substring(pakshaIndex, nakshatraIndex).trim(),
+      fast: line.trim().endsWith("*")
+    }
+
+    // Add the entry to the calendar entries array
+    calendarEntries.push(entry)
+  }
+
+  // Generate the JavaScript code
+  const jsCode = `var cal = ${JSON.stringify(calendarEntries, null, 2)}`
+
+  // Save the JavaScript code to cal.js
+  fs.writeFile('cal.js', jsCode, err => {
+    if (err) {
+      console.error('Error writing file:', err)
+      return
+    }
+    console.log('cal.js file has been created successfully!')
+  })
+})
---
+++ karaner.js
@@ -0,0 +1,90 @@
+const axios = require('axios')
+const { JSDOM } = require('jsdom')
+const moment = require('moment')
+const fs = require('fs')
+
+// Function to fetch the body content from the URL
+async function fetchContent(url) {
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error(`Error fetching content from ${url}:`, error)
+    return null
+  }
+}
+
+// Function to extract the Karana string using jsdom and regex
+function extractKarana(content) {
+  const dom = new JSDOM(content)
+  const body = dom.window.document.querySelector('body')
+  return getKarana(body.textContent)
+}
+
+function getKarana(line) {
+  const pattern = /Karana([A-Za-z]+) upto (\d{1,2}:\d{2} (?:AM|PM))/g
+  const matches = [...line.matchAll(pattern)]
+
+  const karanaArray = matches.map(match => {
+    const karana = match[1] // Extracted Karana value
+    const time = match[2] // Extracted time value
+    return { [karana]: time }
+  })
+
+  return karanaArray
+}
+
+// Function to generate dates from today until December 31, 2025
+function generateDates() {
+  const startDate = moment()
+  const endDate = moment('2050-12-31')
+  const dates = []
+
+  while (startDate.isSameOrBefore(endDate)) {
+    dates.push(startDate.format('DD/MM/YYYY'))
+    startDate.add(1, 'day')
+  }
+
+  return dates
+}
+
+// Main function to fetch content, extract Karana, and create the date: karana object
+async function generateDateKaranaMap() {
+  const baseUrl = 'https://www.drikpanchang.com/panchang/month-panchang.html'
+  const dates = generateDates()
+  const dateKaranaMap = {}
+
+  for (const date of dates) {
+    const url = `${baseUrl}?date=${date}&epoch=${moment().unix()}`
+    console.log(url)
+    const content = await fetchContent(url)
+    const karana = extractKarana(content)
+
+    if(karana.length==0){
+      fs.writeFile('lastdate.txt', moment(date, "DD/MM/YYYY").format("YYYY-MM-DD"), 'utf8', ()=>{})
+      const dom = new JSDOM(content)
+      const body = dom.window.document.querySelector('body')
+      fs.writeFile('content.txt', body.textContent, 'utf8', ()=>{})
+      fs.writeFile('index.html', content, 'utf8', ()=>{})
+      throw new Error("empty karana "+date)
+    }
+
+    dateKaranaMap[moment(date, "DD/MM/YYYY").format("YYYY-MM-DD")] = karana
+    createKaranaFile(dateKaranaMap)
+  }
+  // return dateKaranaMap
+}
+
+function createKaranaFile(dateKaranaMap) {
+  fs.writeFile('karana.json', JSON.stringify(dateKaranaMap, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing karana.json file:', err)
+    }
+  })
+}
+
+// Example usage
+generateDateKaranaMap()
+  .catch(error => {
+    console.error('Error generating date: karana map:', error)
+  })
